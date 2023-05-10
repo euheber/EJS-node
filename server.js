1fb7aa1a4ecfa7eb541be821de30825dc425716f
euheber <euheber1@gmail.com>
2023-05-10 18:24:53 -0300

    feat: POST method to node js server

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
 
 app.set('view engine', 'EJS')
+
 app.use(express.static('public'))
+
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {res.render("index")})
 app.get('/perguntar', (req, res) =>{
     res.render("questions")
 })
+
+app.post('/questionsDBRoute', (req, res) => { 
+    let title = req.body.title
+    let description = req.body.description
+
+    res.send(`${title} e ${description}`)
+})  
 
 app.listen(port)
